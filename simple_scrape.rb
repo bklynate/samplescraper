0e43bf07b63fe0e5239abd01aa03a1ee594879f6
bklynate <nathanielcfa@gmail.com>
2016-01-27 17:49:58 -0500

    simple web scraper built using nokogiri and sinatra

---
+++ simple_scrape.rb
@@ -0,0 +1,32 @@
+require 'sinatra'
+require 'nokogiri'
+require 'open-uri'
+
+url = "http://www.930.com/concerts/"
+data = Nokogiri::HTML(open(url))
+
+concerts = data.css('.tfly-org-id-4')
+
+concerts.each do |concert|
+  puts concert.at_css('.headliners.summary').text.strip
+
+  if !concert.at_css('.doors').nil?
+    puts concert.at_css('.doors').text.strip
+  else
+    puts "No Data"
+  end
+
+  puts concert.at_css('.dates').text.strip
+
+  if !concert.at_css('.custom').nil?
+    puts "SOLD OUT!"
+  else
+    puts concert.at_css('.price-range').text.strip
+  end
+
+  puts ""
+end
+
+
+
+
